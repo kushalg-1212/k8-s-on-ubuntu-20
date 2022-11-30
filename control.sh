25767d1563cf2d4bede88ab4182a68747c5519e8
kushalg-1212 <111343538+kushalg-1212@users.noreply.github.com>
2022-11-30 13:05:56 +0545

    Create control.sh
    Kubernetes Cluster Control Install

---
+++ control.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo apt install apt-transport-https curl -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" >> ~/kubernetes.list
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d
+sudo apt update -y
+sudo apt-get install kubeadm kubelet kubectl -y
+sudo apt-mark hold kubeadm kubelet kubectl
+sudo swapoff -a
+sudo kubeadm init --pod-network-cidr=192.168.120.0/24
